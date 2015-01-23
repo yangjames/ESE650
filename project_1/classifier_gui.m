a872dec26370b65f9ac6b6f0a02345e3aa6716a9
James Yang <james.yang92@gmail.com>
2015-01-22 19:00:22 -0500

    finished classification gui, got a dataset, and starter code for training

--- project_1/classifier_gui.m
+++ project_1/classifier_gui.m
@@ -4,17 +4,22 @@ function classifier_gui
     file_names = {a.name};
     valid_files = ~cellfun(@isempty,regexp(file_names,'^\d+\.\d+\.png\s*$'));
     file_names = file_names(valid_files);
+    for i = 1:length(file_names)
+        distances{i} = file_names{i}(1:end-4);
+    end
+    distances = cellfun(@str2num,distances)';
     im_index = 1;
     
     %% set classification values and pixel coordinate values
-    color_dict = {'red','green','blue','brown','grey','black','yellow'};
+    color_dict = {'barrel','not barrel'};
+    %color_dict = {'red','green','blue','brown','grey','black','yellow'};
     color_idx = 1;
     
     coords = cell(length(file_names),1);
     colors = cell(length(file_names),1);
     
     %% establish figure
-    f = figure('Visible','off');
+    f = figure('Name','Gui Classifier','Visible','off','Position',[100, 100, 1080, 810]);
     ax = axes('Units','pixels');
     
     rgb_im = imread(file_names{im_index});
@@ -31,14 +36,19 @@ function classifier_gui
         'Callback', @get_previous_image);
     
     select_region = uicontrol('Style', 'pushbutton', 'String', 'Select Region',...
-        'Position', [150 20 80 20],...
+        'Position', [200 20 85 20],...
         'Callback', @get_roi);
     
     color_select = uicontrol('Style', 'popup',...
        'String', color_dict,...
-       'Position', [20 340 100 50],...
+       'Position', [290 20 80 20],...
        'Callback', @set_color);
    
+    save_current = uicontrol('Style', 'edit',...
+        'Position', [370 20 250 20],...
+        'Callback', @save_to_file,...
+        'HorizontalAlignment','left');
+        
     f.Visible = 'on';
     
     function get_next_image(source, callbackdata)
@@ -63,13 +73,20 @@ function classifier_gui
 
     function get_roi(source, callbackdata)
         indices = find(roipoly);
-        coords{im_index} = [coords{im_index}; indices]
-        current_color = color_dict{color_idx};
-        colors{im_index} = vertcat(colors{im_index}, repmat({current_color},length(indices),1));
-        colors{im_index}(:);
+        coords{im_index} = [coords{im_index}; indices];
+        %current_color = color_dict{color_idx};
+        colors{im_index} = [colors{im_index}; repmat(color_idx,length(indices),1)];%vertcat(colors{im_index}, repmat({current_color},length(indices),1));
     end
 
     function set_color(source,callbackdata)
         color_idx = source.Value;
+    end
+
+    function save_to_file(source,callbackdata)
+        if ~isempty(source.String)
+            im_size = size(rgb_im);
+            save([source.String '.mat'],'coords','colors','color_dict','file_names','im_size','distances');
+        end
+        set(save_current,'String','');
     end
 end
