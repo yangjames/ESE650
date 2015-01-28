3a4e4a0a1b002c0a2f3c6e76efe3647837d3b5b9
James Yang <james.yang92@gmail.com>
2015-01-28 00:42:33 -0500

    algorithm converges fine. need to see how well it identifies red barrels in test images

--- project_1/project1_v2.m
+++ project_1/project1_v2.m
@@ -1,6 +1,6 @@
 clear all
 clc
-%{
+%{d
 %% load image data
 fprintf('Loading data...\n')
 load barrel_nobarrel_data.mat
@@ -23,42 +23,45 @@ for i = 1:length(file_names)
 end
 data = double(data);
 %}
+%{
 test_mu1 = [0.9 0.5 5];
 test_sigma1 = 0.1*eye(3)
 test_mu2 = [10 5 7];
 test_sigma2 = 0.2*eye(3)
 data = [mvnrnd(test_mu1,test_sigma1,1005); mvnrnd(test_mu2,test_sigma2,1005)];
-
+%}
+%{
 figure(2)
 clf
 %plot(data(:,1),data(:,2),'.')
 plot3(data(:,1),data(:,2),data(:,3),'.')
+xlabel('Y')
+ylabel('Cb')
+zlabel('Cr')
 grid on
 axis equal
-
+drawnow
+%}
 %% create gaussians and initialize GMM
-fprintf('Initializing parameters...\n\n\n\n\n\n\n\n\n\n\n')
+fprintf('Initializing parameters...')
 [n,d] = size(data);
-k = 2;
+k = 4;
 A = cell(k,1);
 mu = zeros(k,d);
 w = zeros(1,k);
 P = zeros(n,k);
-iterator = 1;
-%fprintf('trying: %10d')
-while min(P) <= eps
-    %fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\btrying: %10d',iterator);
-    for i = 1:k
-        A{i} = diag(rand(d,1))*255;
-        w(i) = 1/k;
-        mu(i,:) = rand(1,d)*255;
-        P(:,i) = compute_gaussian_density(data,mu(i,:),A{i});
-    end
-    P(P<eps) = eps;
-    iterator = iterator+1;
+
+for i = 1:k
+    A{i} = diag(rand(d,1))*255;
+    w(i) = 1/k;
+    mu(i,:) = rand(1,d)*255;
+    P(:,i) = compute_gaussian_density(data,mu(i,:),A{i});
 end
+P(P<eps) = eps;
+
 L = 1/n*sum(log(P*w'));
 
+%% start EM
 done = 0;
 gamma = zeros(n,k);
 iterator = 1;
@@ -66,11 +69,6 @@ while ~done
     %fprintf('iteration: %d\n',iterator);
     % E-step
     gamma = bsxfun(@rdivide,bsxfun(@times,w,P),P*w');
-    %{
-    for i = 1:k
-        gamma(:,i) = w(i)*P(:,i)./sum(bsxfun(@times,w,P),2);
-    end
-    %}
     dumb_n = sum(gamma);
     
     % M-step
@@ -87,31 +85,25 @@ while ~done
     L_new = 1/n*sum(log(P*w'));
     
     % check exit condition
-    if abs(L-L_new) < 0.000000000001
+    if abs(L-L_new) < 0.001
         done = 1;
     end
     abs(L_new-L)
     L = L_new;
     iterator = iterator+1;
 end
-%mu
-%A{1}
-%A{2}
-%{d
+    
 %% plot stuff
 figure(1)
 clf
 plot3(data(1:100:end,1),data(1:100:end,2),data(1:100:end,3),'.')
 axis equal
 lims = [0 255];
-%xlim(lims)
-%ylim(lims)
-%zlim(lims)
 grid on
 hold on
 for i = 1:k
     [U,L] = eig(A{i});
-    N = 6;
+    N = 1;
     radii = N*sqrt(diag(L));
     [xc,yc,zc] = ellipsoid(0,0,0,radii(1),radii(2),radii(3));
     a = kron(U(:,1),xc); b = kron(U(:,2),yc); c = kron(U(:,3),zc);
@@ -120,4 +112,4 @@ for i = 1:k
     surf(x,y,z);
     alpha(0.1)
 end
-%}
+drawnow
