e9afc8da38df5b8f33ddf80cf9f511cc82d11d0a
James Yang <james.yang92@gmail.com>
2015-01-29 04:47:12 -0500

    my model plus final testing code

---
+++ project_1/test_detection.m
@@ -0,0 +1,24 @@
+
+dirstruct = dir('*.png');
+
+for i = 1:length(dirstruct),
+
+    % Current test image
+    im = imread(dirstruct(i).name);
+
+    % Your computations here!
+    [x, y, d] = detect_barrel(im);
+
+    % Display results:
+    hf = figure(1);
+    image(im);
+    hold on;
+    plot(x, y, 'g+');
+    title(sprintf('Barrel distance: %.1f m', d));
+
+    % You may also want to plot and display other
+    % diagnostic information such as the outlines
+    % of connected regions, etc.
+    hold off;
+    pause;
+end
