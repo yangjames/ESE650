d58f9111984a7d676639289de960c8548055c4a8
James Yang <james.yang92@gmail.com>
2015-02-08 15:48:04 -0500

    i don't know why, but whatever

--- project_1/test/test_detection.m
+++ project_1/test/test_detection.m
@@ -1,5 +1,8 @@
 clear all
 dirstruct = dir('*.png');
+xf = cell(length(dirstruct),1);
+df = xf;
+yf = df;
 
 for i = 1:length(dirstruct),
 
@@ -9,6 +12,11 @@ for i = 1:length(dirstruct),
     % Run detection algorithm
     [x, y, d] = detect_barrel(im);
 
+    xf{i} = x;
+    yf{i} = y;
+    df{i} = d;
+
+    %{
     % Display results:
     hf = figure(1);
     image(im);
@@ -17,4 +25,11 @@ for i = 1:length(dirstruct),
     title(sprintf('Barrel distance: %.1f m', d));
     hold off;
     pause;
+    %}
+end
+for i = 1:length(xf)
+    fprintf('\niteration: %d\n',i)
+    disp(xf{i})
+    disp(yf{i})
+    disp(df{i})
 end
